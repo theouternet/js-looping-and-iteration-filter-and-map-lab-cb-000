a22a68e676dfdf61017347baccb4606949d92216
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-10 05:59:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function driversWithRevenueOver(array, number) {
 function driverNamesWithRevenueOver(array, number) {
 let arr1 = driversWithRevenueOver(array, number)
 
-let arr2 = arr1.map(e => e[])
+let arr2 = arr1.map(e => e[name])
 
 }
 
